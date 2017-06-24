e81e431d929717169a97fb8213bc9acbb2d3912d
Zachary Sanders <zsanders@icloud.com>
2017-06-24 16:31:36 -0600

    initialize repo

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,43 @@
+class Api::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    render json: Product.all
+  end
+
+  def show
+    render json: @product
+  end
+
+  def create
+    product = Product.new
+    if product.save
+      render json: product
+    else
+      render json: @product.errors.full_messages.join(','), status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params[:id])
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    render json: { message: 'Product deleted'}
+  end
+
+  private
+
+    def product_params
+      params.fetch(:product).permit(:name, :description, :price)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+end
